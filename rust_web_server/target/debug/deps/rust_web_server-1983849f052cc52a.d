d45bd1f509e16cef98f30fdbdcdc137924139fd5
mykk <mykolas.simutis@gmail.com>
2024-08-13 15:53:41 +0300

    rust web server from rust book

---
+++ rust_web_server/target/debug/deps/rust_web_server-1983849f052cc52a.d
@@ -0,0 +1,5 @@
+/home/mykk/source/learning/rust_web_server/target/debug/deps/librust_web_server-1983849f052cc52a.rmeta: src/main.rs
+
+/home/mykk/source/learning/rust_web_server/target/debug/deps/rust_web_server-1983849f052cc52a.d: src/main.rs
+
+src/main.rs:
