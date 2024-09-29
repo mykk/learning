107278610e4b6d14091a680514907144ca147fa9
mykk <mykolas.simutis@gmail.com>
2024-09-29 16:15:56 +0300

    sensors are not as sensitive now

--- micro_bit/lsm303agr/src/main.rs
+++ micro_bit/lsm303agr/src/main.rs
@@ -5,11 +5,8 @@ use cortex_m_rt::entry;
 use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
 
-use microbit::hal::{
-    Timer,
-    uarte::{self, Baudrate, Parity},
-    twim,
-    pac::twim0::frequency::FREQUENCY_A};
+use microbit::{display, hal::{
+    pac::twim0::frequency::FREQUENCY_A, twim, uarte::{self, Baudrate, Parity}, Timer}};
 
 mod serial_setup;
 use serial_setup::UartePort;
@@ -17,6 +14,10 @@ use core::fmt::{self, Write as _};
 use heapless::Vec;
 
 use lsm303agr::{AccelMode, AccelOutputDataRate, MagOutputDataRate, MagMode, Lsm303agr};
+
+fn almost_equal(lhs: i32, rhs: i32, error: i32) -> bool {
+    (lhs - rhs).abs() < error.abs()
+}
 
 #[entry]
 fn main() -> ! {
@@ -41,12 +42,30 @@ fn main() -> ! {
     sensor.set_mag_mode_and_odr(&mut delay, MagMode::HighResolution, MagOutputDataRate::Hz10).unwrap();
     let mut sensor = sensor.into_mag_continuous().ok().unwrap();
 
+    let mut magnometer_values: Option<(i32, i32, i32)> = None;
+    let mut acceleration_values: Option<(i32, i32, i32)> = None;
+
     loop {
         if sensor.mag_status().unwrap().xyz_new_data() {
             sensor.magnetic_field().inspect(|data| {
+                let display = if let Some(previous_vals) = magnometer_values {
+                    !almost_equal(previous_vals.0, data.x_nt(), 1000) || 
+                    !almost_equal(previous_vals.1, data.y_nt(), 1000) ||
+                    !almost_equal(previous_vals.2, data.z_nt(), 1000)
+                }
+                else {
+                    true
+                };
+
+                if !display {
+                    return;
+                }
+
+                magnometer_values = Some((data.x_nt(), data.y_nt(), data.z_nt()));
+
                 let mut data_str = heapless::String::<128>::new();
                 write!(data_str, "Magnetometer: x {} y {} z {}\r\n", data.x_nt(), data.y_nt(), data.z_nt()).unwrap();
-    
+                    
                 nb::block!(match serial.write_str(data_str.as_str()) {
                     Ok(_) => Ok::<(), nb::Error<fmt::Error>>(()),
                     Err(er) => Err(nb::Error::Other(er))
@@ -58,6 +77,20 @@ fn main() -> ! {
 
         if sensor.accel_status().unwrap().xyz_new_data() {
             sensor.acceleration().inspect(|data| {
+                let display = if let Some(previous_vals) = acceleration_values {
+                    !almost_equal(previous_vals.0, data.x_mg(), 1000) || 
+                    !almost_equal(previous_vals.1, data.y_mg(), 1000) ||
+                    !almost_equal(previous_vals.2, data.z_mg(), 1000)
+                }
+                else {
+                    true
+                };
+
+                if !display {
+                    return;
+                }
+                acceleration_values = Some((data.x_mg(), data.y_mg(), data.z_mg()));
+
                 let mut data_str = heapless::String::<128>::new();
                 write!(data_str, "Acceleration: x {} y {} z {}\r\n", data.x_mg(), data.y_mg(), data.z_mg()).unwrap();
     
