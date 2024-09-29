b4ff49ec7cf9f45ab6d1ba6d79c31ba0770c346f
mykk <mykolas.simutis@gmail.com>
2024-09-29 18:24:11 +0300

    led compass

--- micro_bit/led_compass/src/main.rs
+++ micro_bit/led_compass/src/main.rs
@@ -1,21 +1,28 @@
 #![no_main]
 #![no_std]
 
+use core::f32::consts::PI;
+
 use cortex_m_rt::entry;
+use libm::atan2f;
 use lsm303agr::AccelMode;
 use lsm303agr::MagMode;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 mod calibration;
-use crate::calibration::calc_calibration;
-use crate::calibration::calibrated_measurement;
+use calibration::calc_calibration;
+use calibration::calibrated_measurement;
 
 use microbit::{display::blocking::Display, hal::Timer};
 
 use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
 
 use lsm303agr::{AccelOutputDataRate, Lsm303agr, MagOutputDataRate};
+
+mod led;
+use led::Direction;
+use led::direction_to_led;
 
 #[entry]
 fn main() -> ! {
@@ -36,12 +43,35 @@ fn main() -> ! {
     let calibration = calc_calibration(&mut sensor, &mut display, &mut timer);
     rprintln!("Calibration: {:?}", calibration);
     rprintln!("Calibration done, entering busy loop");
+
     loop {
         while !sensor.mag_status().unwrap().xyz_new_data() {}
+        let data = sensor.magnetic_field().unwrap();
+        let data = calibrated_measurement(data, &calibration);
 
-        sensor.magnetic_field().inspect(|data| {
-            let data = calibrated_measurement(*data, &calibration);
-            rprintln!("x: {}, y: {}, z: {}", data.x, data.y, data.z);
-        }).unwrap();
+        let theta = atan2f(data.y as f32, data.x as f32);
+
+        let dir = if theta < -7. * PI / 8. {
+            Direction::West
+        } else if theta < -5. * PI / 8. {
+            Direction::SouthWest
+        } else if theta < -3. * PI / 8. {
+            Direction::South
+        } else if theta < -PI / 8. {
+            Direction::SouthEast
+        } else if theta < PI / 8. {
+            Direction::East
+        } else if theta < 3. * PI / 8. {
+            Direction::NorthEast
+        } else if theta < 5. * PI / 8. {
+            Direction::North
+        } else if theta < 7. * PI / 8. {
+            Direction::NorthWest
+        } else {
+            Direction::West
+        };
+
+        display.show(&mut timer, direction_to_led(dir), 100);
     }
+
 }
